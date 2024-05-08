541dab65c155f527c0f8d39a38b4a9e4088b1976
nebulous42069 <125807561+nebulous42069@users.noreply.github.com>
2024-05-08 09:35:07 -0400

    update

---
+++ omega/script.module.xenonpluss/resources/lib/modules/widget_switch.py
@@ -0,0 +1,87 @@
+import os
+import shutil
+import subprocess
+import xbmcgui
+import xbmcvfs
+import xbmc, xbmcaddon
+dialog = xbmcgui.Dialog()
+settings_path = xbmcvfs.translatePath('special://userdata/addon_data/skin.xenonplus/')
+
+def Xenon_Widget_Flavors():
+        flavor_list = ['[COLOR chartreuse]Xenon Plus-Restore Default Setup (TMDBH)[/COLOR]', 'Xenon Plus-Umbrella (Debrid Only)', 'Xenon Plus-Seren (Debrid Only)', 'Xenon Plus-Afennity (Debrid Only)', 'Xenon FREE']
+        select = dialog.select('Choose Your Xenon Flavor!',flavor_list)
+        if select == None:
+            return
+            
+        if select == 0:
+                src_settings = xbmcvfs.translatePath('special://home/addons/script.module.xenonpluss/resources/switch/Xenon_Plus_Default/settings.xml')
+                dst_settings = xbmcvfs.translatePath('special://userdata/addon_data/skin.xenonplus/settings.xml')
+                
+                if os.path.exists(os.path.join(settings_path)):
+                        try:
+                                shutil.copyfile(src_settings, dst_settings)
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'To save skin changes, please close Kodi, Press OK to force close Kodi')
+                                os._exit(1)
+                        except:
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                else:
+                        xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+   
+                        
+        if select == 1:
+                src_settings = xbmcvfs.translatePath('special://home/addons/script.module.xenonpluss/resources/switch/Xenon_Plus_Umbrella/settings.xml')
+                dst_settings = xbmcvfs.translatePath('special://userdata/addon_data/skin.xenonplus/settings.xml')
+                
+                if os.path.exists(os.path.join(settings_path)):
+                        try:
+                                shutil.copyfile(src_settings, dst_settings)
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'To save skin changes, please close Kodi, Press OK to force close Kodi')
+                                os._exit(1)
+                        except:
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                else:
+                        xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer') 
+
+        if select == 2:
+                src_settings = xbmcvfs.translatePath('special://home/addons/script.module.xenonpluss/resources/switch/Xenon_Plus_Seren/settings.xml')
+                dst_settings = xbmcvfs.translatePath('special://userdata/addon_data/skin.xenonplus/settings.xml')
+                
+                if os.path.exists(os.path.join(settings_path)):
+                        try:
+                                shutil.copyfile(src_settings, dst_settings)
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'To save skin changes, please close Kodi, Press OK to force close Kodi')
+                                os._exit(1)
+                        except:
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                else:
+                        xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+
+        if select == 3:
+                src_settings = xbmcvfs.translatePath('special://home/addons/script.module.xenonpluss/resources/switch/Xenon_Plus_Afennity/settings.xml')
+                dst_settings = xbmcvfs.translatePath('special://userdata/addon_data/skin.xenonplus/settings.xml')
+                
+                if os.path.exists(os.path.join(settings_path)):
+                        try:
+                                shutil.copyfile(src_settings, dst_settings)
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'To save skin changes, please close Kodi, Press OK to force close Kodi')
+                                os._exit(1)
+                        except:
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                else:
+                        xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                        
+        if select == 4:
+                src_settings = xbmcvfs.translatePath('special://home/addons/script.module.xenonpluss/resources/switch/Xenon_Plus_FREE/settings.xml')
+                dst_settings = xbmcvfs.translatePath('special://userdata/addon_data/skin.xenonplus/settings.xml')
+                
+                if os.path.exists(os.path.join(settings_path)):
+                        try:
+                                shutil.copyfile(src_settings, dst_settings)
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'To save skin changes, please close Kodi, Press OK to force close Kodi')
+                                os._exit(1)
+                        except:
+                                xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                else:
+                        xbmcgui.Dialog().ok('Xenon Plus Switcher', 'Error switching skin, please contact developer')
+                        
+                 
